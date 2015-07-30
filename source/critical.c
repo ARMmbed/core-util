8ed5535355e18134d2c98b0d2b62df4c3d6eb878
Brendan Moran <brendan.moran@arm.com>
2015-07-30 10:57:29 +0100

    Add a C re-entrant critical section API

---
+++ source/critical.c
@@ -0,0 +1,40 @@
+/*
+ * PackageLicenseDeclared: Apache-2.0
+ * Copyright (c) 2015 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <stdint.h>                  // uint32_t, UINT32_MAX
+#include <stddef.h>                  // NULL
+#include "cmsis-core/core_generic.h" //__disable_irq, __enable_irq
+
+// Module include
+#include "mbed-util/critical.h"
+
+static volatile uint32_t interruptEnableCounter = 0;
+void critical_section_enter()
+{
+    __disable_irq();
+    /* not allowed to overflow the interruptEnableCounter. */
+    if (interruptEnableCounter == UINT32_MAX) {
+        /* Generate a fault */
+        *((uint32_t *)NULL) = 0;
+    }
+    interruptEnableCounter++;
+}
+
+void critical_section_exit()
+{
+    if (interruptEnableCounter) interruptEnableCounter--;
+    if (!interruptEnableCounter) __enable_irq();
+}
---
+++ test/critical.cpp
@@ -0,0 +1,55 @@
+/*
+ * PackageLicenseDeclared: Apache-2.0
+ * Copyright (c) 2015 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cmsis-core/core_generic.h" // __get_PRIMASK
+#include "mbed/test_env.h"           // MBED_HOSTTEST_* definitions
+// Module include
+#include "mbed-util/critical.h"
+
+void app_start(int argc, char *argv[])
+{
+    MBED_HOSTTEST_TIMEOUT(5);
+    MBED_HOSTTEST_SELECT(default);
+    MBED_HOSTTEST_DESCRIPTION(mbed-util critical section test);
+    MBED_HOSTTEST_START("MBED_UTIL_CRITICAL");
+    bool pass = false;
+    do {
+        // Check the current primask
+        uint32_t primask;
+        if ((primask = __get_PRIMASK()) != 0) break;
+
+        // Test a single critical section
+        mbed::util::critical_section_enter();
+        if ((primask = __get_PRIMASK()) == 0) break;
+        mbed::util::critical_section_exit();
+        if ((primask = __get_PRIMASK()) != 0) break;
+
+        // Test multiple nested critical sections
+        for (int i = 0; i < 16; i++) {
+            mbed::util::critical_section_enter();
+        }
+        if ((primask = __get_PRIMASK()) == 0) break;
+        for (int i = 0; i < 16; i++) {
+            mbed::util::critical_section_exit();
+        }
+        if ((primask = __get_PRIMASK()) != 0) break;
+
+        pass = true;
+    } while (0);
+
+    MBED_HOSTTEST_RESULT(pass);
+}
