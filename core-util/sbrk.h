b00a8771386b21e32750710ee5e8eea20a73a79d
Rohit Grover <rohit.grover@arm.com>
2015-09-22 11:44:32 +0100

    add a level of indirection in accessing mbed_sbrk_start (and similar) to support POSIX

--- core-util/sbrk.h
+++ core-util/sbrk.h
@@ -37,7 +37,11 @@
 #   define STDERR_FILENO    2
 
 #else
-#   include <sys/syslimits.h>
+#   ifndef TARGET_LIKE_POSIX
+#       include <sys/syslimits.h>
+#   else
+#       include <stddef.h>
+#   endif
 #   define PREFIX(x)    x
 #endif
 
@@ -70,23 +74,100 @@
 #define KRBS_INC_MIN (KRBS_ALIGN)
 #endif
 
-#ifndef MBED_HEAP_SIZE
-extern uint32_t __heap_size;
-// Extract linker heap size parameter
-#define MBED_HEAP_SIZE ((ptrdiff_t) &__heap_size)
-#endif
+/**
+ * Here's a picture to explain the mbed OS memory layout. It should help with
+ * some of the concepts below.
+ *
+ *            | inaccessible |
+ *            | or RAM-end   |
+ * high addrs +--------------+  +--+--+
+ *            |  krbs_start  |     |
+ *            |      +       |     |
+ *            |      |       |     |
+ *            |      |       |     |
+ *            |      v       |     |
+ *            |              |     |
+ *            |  Free store  |  heap_size
+ *            |              |     |
+ *            |      ^       |     |
+ *            |      |       |     |
+ *            |      |       |     |
+ *            |      +       |     |
+ *            |  sbrk_start  |     |
+ *            +--------------+  +--+--+
+ *            |              |
+ *            |   BSS        |
+ *            |              |
+ *            +--------------+
+ *            |              |
+ *            |   Data       |
+ *            |              |
+ *            +---------+----+
+ *            |         |    |
+ *            |   stack |    |
+ *            |         v    |
+ *            |              |
+ *  low addrs +--------------+
+ *            | inaccessible |
+ *            | or RAM-start |
+ */
 
-#ifdef __ARMCC_VERSION
-	#define __mbed_sbrk_start (Image$$ARM_LIB_HEAP$$Base)
-	#define __mbed_krbs_start (Image$$ARM_LIB_HEAP$$ZI$$Limit)
-	#define __heap_size (Image$$ARM_LIB_HEAP$$ZI$$Length)
-	extern unsigned int __mbed_sbrk_start;
-	extern unsigned int __mbed_krbs_start;
-	extern unsigned int __heap_size;
-	#pragma import(__use_two_region_memory)
-#else
-	extern uint32_t __mbed_krbs_start;
-	extern uint32_t __mbed_sbrk_start;
+/**
+ * __heap_size is a symbol defined by the yotta target to contain the size of
+ * the free-store. In mbed OS, this is typically defined in the linker script
+ * associated with the target. The value is resolved at link time and is
+ * available within the program using the const R-value expression `&__heap_size`.
+ *
+ * For some targets, __heap_size may also be given a value programmatically by
+ * the application during system startup; in this case, it is available
+ * as a regular extern const L-value symbol.
+ *
+ * The macro MBED_HEAP_SIZE adds a level of indirection to abstract __heap_size.
+ */
+#ifndef MBED_HEAP_SIZE
+    #ifndef TARGET_LIKE_POSIX
+        #ifdef __ARMCC_VERSION
+            #define __heap_size (Image$$ARM_LIB_HEAP$$ZI$$Length)
+        #endif
+        extern unsigned int __heap_size;
+        #define MBED_HEAP_SIZE ((ptrdiff_t)&__heap_size)
+    #else
+        extern unsigned int __heap_size;
+        #define MBED_HEAP_SIZE ((ptrdiff_t) __heap_size)
+    #endif
+#endif /* #ifndef MBED_HEAP_SIZE */
+
+/**
+ * The symbols __mbed_sbrk_start and __mbed_krbs_start are defined by the yotta-
+ * target to mark the boundary of the free-store. In mbed OS, these are
+ * typically defined in the linker script associated with the target. The
+ * values are resolved at link time and are available within the program using
+ * toolchain specific const R-value expressions.
+ *
+ * For some targets, sbrk_start and krbs_start may also be given a value
+ * programmatically by the application during system startup; in this case,
+ * these are available as a regular extern const L-value symbols.
+ *
+ * The macros MBED_SBRK_START and MBED_KRBS_START add a level of indirection to
+ * abstract these symbols.
+ */
+#if !defined(MBED_SBRK_START) || !defined(MBED_KRBS_START)
+    #ifndef TARGET_LIKE_POSIX
+        #ifdef __ARMCC_VERSION
+            #define __mbed_sbrk_start (Image$$ARM_LIB_HEAP$$Base)
+            #define __mbed_krbs_start (Image$$ARM_LIB_HEAP$$ZI$$Limit)
+            #pragma import(__use_two_region_memory)
+        #endif /*#ifdef __ARMCC_VERSION */
+        extern unsigned int __mbed_sbrk_start;
+        extern unsigned int __mbed_krbs_start;
+        #define MBED_SBRK_START &__mbed_sbrk_start
+        #define MBED_KRBS_START &__mbed_krbs_start
+    #else /* #ifdef TARGET_LIKE_POSIX */
+        extern void *__mbed_sbrk_start;
+        extern void *__mbed_krbs_start;
+        #define MBED_SBRK_START __mbed_sbrk_start
+        #define MBED_KRBS_START __mbed_krbs_start
+    #endif /* #ifdef TARGET_LIKE_POSIX */
 #endif
 
 #ifdef __cplusplus
