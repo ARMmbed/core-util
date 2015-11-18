7866dbe082218313df6ff43a17c211323479b520
Rohit Grover <rohit.grover@arm.com>
2015-11-18 13:15:28 +0000

    use of uintptr_t instead of uint32_t*

--- source/PoolAllocator.cpp
+++ source/PoolAllocator.cpp
@@ -32,12 +32,12 @@ PoolAllocator::PoolAllocator(void *start, size_t elements, size_t element_size, 
 }
 
 void* PoolAllocator::alloc() {
-    uint32_t prev_free = (uint32_t)_free_block;
+    uintptr_t prev_free = reinterpret_cast<uintptr_t>(_free_block);
     if (0 == prev_free)
         return NULL;
     while (true) {
         void **const new_free = (void **)(*((void **)prev_free));
-        if (atomic_cas((uint32_t*)&_free_block, &prev_free, (uint32_t)new_free)) {
+        if (atomic_cas((uintptr_t*)&_free_block, &prev_free, (uintptr_t)new_free)) {
             return (void*)prev_free;
         }
     }
@@ -45,10 +45,10 @@ void* PoolAllocator::alloc() {
 
 void PoolAllocator::free(void* p) {
     if (owns(p)) {
-        uint32_t prev_free = (uint32_t)_free_block;
+        uintptr_t prev_free = reinterpret_cast<uintptr_t>(_free_block);
         while (true) {
             *((void**)p) = (void*)prev_free;
-            if (atomic_cas((uint32_t*)&_free_block, &prev_free, (uint32_t)p)) {
+            if (atomic_cas((uintptr_t*)&_free_block, &prev_free, (uintptr_t)p)) {
                 break;
             }
         }
