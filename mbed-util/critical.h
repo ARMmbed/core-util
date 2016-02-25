f6654d17acbe1451ecb79946d66e8859cb146e4f
Anna Bridge <Anna.Bridge@arm.com>
2016-02-25 15:44:00 +0000

    Code review: Updated licence headers, corrected Doxygen function headers, minor code tweaks

--- mbed-util/critical.h
+++ mbed-util/critical.h
@@ -1,16 +1,16 @@
 /*
- * PackageLicenseDeclared: Apache-2.0
- * Copyright (c) 2015 ARM Limited
+ * Copyright (c) 2015-2016, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -19,37 +19,35 @@
 #define __MBED_UTIL_CRITICAL_H__
 
 #ifdef __cplusplus
-    namespace mbed {
-    namespace util {
-    extern "C" {
+namespace mbed {
+namespace util {
+extern "C" {
 #endif
 
-#include <assert.h>
-
-/*
- * critical_section_enter
- *
- * This function should be called to mark the start of a critical section of code.
- * NOTES:
- * 1) The use of this style of critical section is targetted at C based implementations.
- * 2) These critical sections can be nested.
- * 3) The interrupt enable state on entry to the first critical section (of a nested set, or single
- *    section) will be preserved on exit from the section.
- * 4) This implementation will currently only work on code running in privileged mode.
- */
+/** Mark the start of a critical section
+  *
+  * This function should be called to mark the start of a critical section of code.
+  * \note
+  * NOTES:
+  * 1) The use of this style of critical section is targetted at C based implementations.
+  * 2) These critical sections can be nested.
+  * 3) The interrupt enable state on entry to the first critical section (of a nested set, or single
+  *    section) will be preserved on exit from the section.
+  * 4) This implementation will currently only work on code running in privileged mode.
+  */
 void critical_section_enter();
 
-/*
- * critical_section_exit
- *
- * This function should be called to mark the end of a critical section of code.
- * NOTES:
- * 1) The use of this style of critical section is targetted at C based implementations.
- * 2) These critical sections can be nested.
- * 3) The interrupt enable state on entry to the first critical section (of a nested set, or single
- *    section) will be preserved on exit from the section.
- * 4) This implementation will currently only work on code running in privileged mode.
- */
+/** Mark the end of a critical section
+  *
+  * This function should be called to mark the end of a critical section of code.
+  * \note
+  * NOTES:
+  * 1) The use of this style of critical section is targetted at C based implementations.
+  * 2) These critical sections can be nested.
+  * 3) The interrupt enable state on entry to the first critical section (of a nested set, or single
+  *    section) will be preserved on exit from the section.
+  * 4) This implementation will currently only work on code running in privileged mode.
+  */
 void critical_section_exit();
 
 #ifdef __cplusplus
--- source/critical.c
+++ source/critical.c
@@ -1,22 +1,23 @@
 /*
- * PackageLicenseDeclared: Apache-2.0
- * Copyright (c) 2015 ARM Limited
+ * Copyright (c) 2015-2016, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 #include <stdint.h>                  // uint32_t, UINT32_MAX
 #include <stddef.h>                  // NULL
 #include "cmsis-core/core_generic.h" //__disable_irq, __enable_irq
+#include <assert.h>
 
 // Module include
 #include "mbed-util/critical.h"
@@ -38,19 +39,27 @@ void critical_section_enter()
     // are enabled, then something has gone badly wrong thus assert an error.
 
     assert(interruptEnableCounter < UINT32_MAX);
-    if (interruptEnableCounter > 0) assert(primask & 0x1);
-
+    if (interruptEnableCounter > 0) {
+        assert(primask & 0x1);
+    }
     interruptEnableCounter++;
 }
 
 void critical_section_exit()
 {
-    uint32_t primask = __get_PRIMASK(); // get the current interrupt enabled state
-    assert(primask & 0x1); // Interrupts must be disabled on invoking an exit from a critical section
+    // If critical_section_enter has not previously been called, do nothing
+    if (interruptEnableCounter) {
 
-    if (interruptEnableCounter) interruptEnableCounter--;
+        uint32_t primask = __get_PRIMASK(); // get the current interrupt enabled state
 
-    // Only re-enable interrupts if we are exiting the last of the nested critical sections and
-    // interrupts were enabled on entry to the first critical section.
-    if (!interruptEnableCounter && !critical_primask) __enable_irq();
+        assert(primask & 0x1); // Interrupts must be disabled on invoking an exit from a critical section
+
+        interruptEnableCounter--;
+
+        // Only re-enable interrupts if we are exiting the last of the nested critical sections and
+        // interrupts were enabled on entry to the first critical section.
+        if (!interruptEnableCounter && !critical_primask) {
+            __enable_irq();
+        }
+    }
 }
